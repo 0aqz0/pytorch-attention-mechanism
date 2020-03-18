bac922e75f91bb106726ce91a3a0388d54a34ceb
0aqz0 <18026332256@163.com>
2020-03-18 14:43:35 +0800

    visualize attention map

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # pytorch-attention-mechanism
 my codes for learning attention mechanism
 
+## CNN with attention
+
+### Usage
+
+Train the model:
+
+```bash
+$ python cnn-with-attention.py --train
+```
+
+Visualize attention map:
+
+```bash
+$ python cnn-with-attention.py --visualize
+```
+
 ## Todos
 
-- [ ] CNN+attention
+- [x] CNN+attention
 - [ ] RNN+attention
 
+## References
+
+- [Learn to Pay Attention! Trainable Visual Attention in CNNs](https://towardsdatascience.com/learn-to-pay-attention-trainable-visual-attention-in-cnns-87e2869f89f1)
+
+- [Attention in Neural Networks and How to Use It](http://akosiorek.github.io/ml/2017/10/14/visual-attention.html)
+
+- [Attention? Attention!](https://lilianweng.github.io/lil-log/2018/06/24/attention-attention.html)
--- cnn-with-attention.py
+++ cnn-with-attention.py
@@ -4,12 +4,13 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
+import torchvision.utils as utils
 from tensorboardX import SummaryWriter
 import os
 import argparse
 from datetime import datetime
 from models import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
-from functions import train_epoch, test
+from functions import train_epoch, test, visualize_attn
 
 # Parameters manager
 parser = argparse.ArgumentParser(description='CNN with Attention')
@@ -23,11 +24,13 @@ parser.add_argument('--save_path', default='./', type=str,
     help='Path to save the model')
 parser.add_argument('--visualize', action='store_true',
     help='Visualize the attention vector')
-parser.add_argument('--epochs', default=100, type=int,
+parser.add_argument('--checkpoint', default='checkpoint.pth', type=str,
+    help='Path to checkpoint')
+parser.add_argument('--epochs', default=200, type=int,
     help='Epochs for training')
 parser.add_argument('--batch_size', default=32, type=int,
     help='Batch size for training or testing')
-parser.add_argument('--lr', default=1e-1, type=float,
+parser.add_argument('--lr', default=1e-4, type=float,
     help='Learning rate for training')
 parser.add_argument('--device', default='0', type=str,
     help='Cuda device to use')
@@ -66,12 +69,13 @@ if __name__ == '__main__':
     if torch.cuda.device_count() > 1:
         print("Using {} GPUs".format(torch.cuda.device_count()))
         model = nn.DataParallel(model)
+    # Summary writer
+    writer = SummaryWriter("runs/cnn_attention_{:%Y-%m-%d_%H-%M-%S}".format(datetime.now()))
     # Train
     if args.train:
-        # Create loss criterion & optimizer & writer
+        # Create loss criterion & optimizer
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        writer = SummaryWriter("runs/cnn_attention_{:%Y-%m-%d_%H-%M-%S}".format(datetime.now()))
 
         for epoch in range(args.epochs):
             train_epoch(model, criterion, optimizer, train_loader, device, epoch, args.log_interval, writer)
@@ -81,4 +85,26 @@ if __name__ == '__main__':
 
     # Visualize
     if args.visualize:
-        pass
+        # Load model
+        model.load_state_dict(torch.load(args.checkpoint))
+        model.eval()
+
+        with torch.no_grad():
+            for batch_idx, (inputs, labels) in enumerate(test_loader):
+                # get images
+                inputs = inputs.to(device)
+                if batch_idx == 0:
+                    images = inputs[0:16,:,:,:]
+                    I = utils.make_grid(images, nrow=4, normalize=True, scale_each=True)
+                    writer.add_image('origin', I)
+                    _, c1, c2, c3, c4 = model(images)
+                    # print(I.shape, c1.shape, c2.shape, c3.shape, c4.shape)
+                    attn1 = visualize_attn(I, c1)
+                    writer.add_image('attn1', attn1)
+                    attn2 = visualize_attn(I, c2)
+                    writer.add_image('attn2', attn2)
+                    attn3 = visualize_attn(I, c3)
+                    writer.add_image('attn3', attn3)
+                    attn4 = visualize_attn(I, c4)
+                    writer.add_image('attn4', attn4)
+                    break
