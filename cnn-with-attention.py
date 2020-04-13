1c3ce40dc6784b27b70596a59e4876a3f23c9619
0aqz0 <18026332256@163.com>
2020-04-13 21:44:15 +0800

    train the model

--- cnn-with-attention.py
+++ cnn-with-attention.py
@@ -11,29 +11,32 @@ import os
 import argparse
 import numpy as np
 from datetime import datetime
-from models import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
-from functions import train_epoch, test, visualize_attn
+from models import AttnVGG
+from functions import train_epoch, val_epoch, visualize_attn
+
 
 # Parameters manager
 parser = argparse.ArgumentParser(description='CNN with Attention')
 parser.add_argument('--train', action='store_true',
     help='Train the network')
-parser.add_argument('--attention', action='store_true',
-    help='Train with attention')
-parser.add_argument('--save', action='store_true',
-    help='Save the model')
-parser.add_argument('--save_path', default='/home/haodong/Data/attention_models', type=str,
-    help='Path to save the model')
 parser.add_argument('--visualize', action='store_true',
     help='Visualize the attention vector')
+parser.add_argument('--no_attention', action='store_true',
+    help='Train without attention')
+parser.add_argument('--no_save', action='store_true',
+    help='Not save the model')
+parser.add_argument('--save_path', default='/home/haodong/Data/attention_models', type=str,
+    help='Path to save the model')
 parser.add_argument('--checkpoint', default='checkpoint.pth', type=str,
     help='Path to checkpoint')
 parser.add_argument('--epochs', default=300, type=int,
     help='Epochs for training')
-parser.add_argument('--batch_size', default=16, type=int,
+parser.add_argument('--batch_size', default=32, type=int,
     help='Batch size for training or testing')
 parser.add_argument('--lr', default=1e-4, type=float,
     help='Learning rate for training')
+parser.add_argument('--weight_decay', default=1e-4, type=float,
+    help='Weight decay for training')
 parser.add_argument('--device', default='0', type=str,
     help='Cuda device to use')
 parser.add_argument('--log_interval', default=100, type=int,
@@ -45,9 +48,6 @@ os.environ["CUDA_VISIBLE_DEVICES"]=args.device
 # Device setting
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# hyperparams
-batch_size = args.batch_size
-learning_rate = args.lr
 
 if __name__ == '__main__':
     # Load data
@@ -62,11 +62,13 @@ if __name__ == '__main__':
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
     train_set = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=16)
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=16)
     test_set = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=16)
+    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=True, num_workers=16)
     # Create model
-    model = ResNet18(pretrained=True, progress=True, attention=args.attention, num_classes=100).to(device)
+    if not args.no_attention:
+        print('Using Attention Model')
+    model = AttnVGG(sample_size=32, num_classes=100, attention=not args.no_attention).to(device)
     # Run the model parallelly
     if torch.cuda.device_count() > 1:
         print("Using {} GPUs".format(torch.cuda.device_count()))
@@ -77,17 +79,18 @@ if __name__ == '__main__':
     if args.train:
         # Create loss criterion & optimizer
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
         # lr_lambda = lambda epoch : np.power(0.5, int(epoch/25))
         # scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
 
         for epoch in range(args.epochs):
             train_epoch(model, criterion, optimizer, train_loader, device, epoch, args.log_interval, writer)
-            test(model, criterion, test_loader, device, epoch, writer)
+            val_epoch(model, criterion, test_loader, device, epoch, writer)
             # adjust learning rate
             # scheduler.step()
-            if args.save:
-                torch.save(model.state_dict(), os.path.join(args.save_path, "epoch{:03d}.pth".format(epoch+1)))
+            if not args.no_save:
+                torch.save(model.state_dict(), os.path.join(args.save_path, "cnn_epoch{:03d}.pth".format(epoch+1)))
+                print("Saving Model of Epoch {}".format(epoch+1))
 
     # Visualize
     if args.visualize:
