631a7d7f3c0fb3b784566211e38ce7763378f324
0aqz0 <18026332256@163.com>
2020-04-13 13:51:06 +0800

    add vgg16 with attention

--- attention.py
+++ attention.py
@@ -6,92 +6,36 @@ import torch.nn.functional as F
 Attention blocks
 Reference: Learn To Pay Attention
 """
-# class ProjectorBlock(nn.Module):
-#     def __init__(self, in_features, out_features):
-#         super(ProjectorBlock, self).__init__()
-#         self.op = nn.Conv2d(in_channels=in_features, out_channels=out_features,
-#             kernel_size=1, padding=0, bias=False)
-
-#     def forward(self, x):
-#         return self.op(x)
-
-
-# class LinearAttentionBlock(nn.Module):
-#     def __init__(self, in_features, normalize_attn=True):
-#         super(LinearAttentionBlock, self).__init__()
-#         self.normalize_attn = normalize_attn
-#         self.op = nn.Conv2d(in_channels=in_features, out_channels=1,
-#             kernel_size=1, padding=0, bias=False)
-
-#     def forward(self, l, g):
-#         N, C, H, W = l.size()
-#         c = self.op(l+g) # (batch_size,1,H,W)
-#         if self.normalize_attn:
-#             a = F.softmax(c.view(N,1,-1), dim=2).view(N,1,H,W)
-#         else:
-#             a = torch.sigmoid(c)
-#         g = torch.mul(a.expand_as(l), l)
-#         if self.normalize_attn:
-#             g = g.view(N,C,-1).sum(dim=2) # (batch_size,C)
-#         else:
-#             g = F.adaptive_avg_pool2d(g, (1,1)).view(N,C)
-#         return c.view(N,1,H,W), g
-
-"""
-Spatial & Channel Attention Blocks
-Reference: BAM: Bottleneck Attention Module (BMVC2018)
-"""
-class ChannelAttn(nn.Module):
-    def __init__(self, in_channels, reduction_ratio=16):
-        super(ChannelAttn, self).__init__()
-        self.fc1 = nn.Linear(in_channels, in_channels//reduction_ratio)
-        self.bn1 = nn.BatchNorm1d(in_channels//reduction_ratio)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(in_channels//reduction_ratio, in_channels)
+class ProjectorBlock(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(ProjectorBlock, self).__init__()
+        self.op = nn.Conv2d(in_channels=in_features, out_channels=out_features,
+            kernel_size=1, padding=0, bias=False)
 
     def forward(self, x):
-        out = F.adaptive_avg_pool2d(x, (1,1))
-        out = out.view(out.size(0), -1)
-        # print(out.shape)
-        out = self.relu(self.bn1(self.fc1(out)))
-        out = self.fc2(out)
-        # print(out.shape)
-        return out.unsqueeze(2).unsqueeze(3).expand_as(x)
+        return self.op(x)
+
 
 class SpatialAttn(nn.Module):
-    def __init__(self, in_channels, reduction_ratio=16):
+    def __init__(self, in_features, normalize_attn=True):
         super(SpatialAttn, self).__init__()
-        # 1x1 conv
-        self.conv1 = nn.Conv2d(in_channels, in_channels//reduction_ratio, kernel_size=1)
-        self.bn1 = nn.BatchNorm2d(in_channels//reduction_ratio)
-        self.relu = nn.ReLU()
-        # 3x3 conv * 2
-        self.conv2 = nn.Conv2d(in_channels//reduction_ratio, in_channels//reduction_ratio, kernel_size=3,
-                                padding=4, dilation=4)
-        self.bn2 = nn.BatchNorm2d(in_channels//reduction_ratio)
-        self.conv3 = nn.Conv2d(in_channels//reduction_ratio, in_channels//reduction_ratio, kernel_size=3,
-                                padding=4, dilation=4)
-        self.bn3 = nn.BatchNorm2d(in_channels//reduction_ratio)
-        # 1x1 conv
-        self.conv4 = nn.Conv2d(in_channels//reduction_ratio, 1, kernel_size=1)
+        self.normalize_attn = normalize_attn
+        self.op = nn.Conv2d(in_channels=in_features, out_channels=1,
+            kernel_size=1, padding=0, bias=False)
 
-    def forward(self, x):
-        out = self.relu(self.bn1(self.conv1(x)))
-        out = self.relu(self.bn2(self.conv2(out)))
-        out = self.relu(self.bn3(self.conv3(out)))
-        out = self.conv4(out)
-        return out.expand_as(x)
-
-class AttentionBlock(nn.Module):
-    def __init__(self, in_channels):
-        super(AttentionBlock, self).__init__()
-        self.channel_attn = ChannelAttn(in_channels)
-        self.spatial_attn = SpatialAttn(in_channels)
-
-    def forward(self, x):
-        attn = 1 + torch.sigmoid(self.channel_attn(x) + self.spatial_attn(x))
-        # print(attn.shape)
-        return attn * x
+    def forward(self, l, g):
+        N, C, H, W = l.size()
+        c = self.op(l+g) # (batch_size,1,H,W)
+        if self.normalize_attn:
+            a = F.softmax(c.view(N,1,-1), dim=2).view(N,1,H,W)
+        else:
+            a = torch.sigmoid(c)
+        g = torch.mul(a.expand_as(l), l)
+        if self.normalize_attn:
+            g = g.view(N,C,-1).sum(dim=2) # (batch_size,C)
+        else:
+            g = F.adaptive_avg_pool2d(g, (1,1)).view(N,C)
+        return c.view(N,1,H,W), g
 
 """
 Temporal attention block
@@ -124,23 +68,11 @@ class TemporalAttn(nn.Module):
 
 # Test
 if __name__ == '__main__':
-    # # 2d block
-    # attention_block = LinearAttentionBlock(in_features=3)
-    # l = torch.randn(16, 3, 128, 128)
-    # g = torch.randn(16, 3, 128, 128)
-    # print(attention_block(l, g))
-    # channel block
-    channel_block = ChannelAttn(in_channels=128)
-    x = torch.randn(16, 128, 8, 8)
-    print(channel_block(x).shape)
-    # spatial block
-    spatial_block = SpatialAttn(in_channels=128)
-    x = torch.randn(16, 128, 8, 8)
-    print(spatial_block(x).shape)
-    # attn block
-    attn_block = AttentionBlock(in_channels=128)
-    x = torch.randn(16, 128, 8, 8)
-    print(attn_block(x).shape)
+    # 2d block
+    spatial_block = SpatialAttn(in_features=3)
+    l = torch.randn(16, 3, 128, 128)
+    g = torch.randn(16, 3, 128, 128)
+    print(spatial_block(l, g))
     # temporal block
     temporal_block = TemporalAttn(hidden_size=256)
     x = torch.randn(16, 30, 256)
--- models.py
+++ models.py
@@ -1,224 +1,84 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.hub import load_state_dict_from_url
-from attention import AttentionBlock, TemporalAttn
+from attention import ProjectorBlock, SpatialAttn, TemporalAttn
 import math
 
 """
-Implementation of ResNet
-Reference: Deep Residual Learning for Image Recognition
+VGG-16 with attention
 """
-class BasicBlock(nn.Module):
-    expansion = 1
-    # planes refer to the number of feature maps
-    def __init__(self, inplanes, planes, stride=1, downsample=None, attention=False):
-        super(BasicBlock, self).__init__()
-        self.downsample = downsample
-        self.conv1 = nn.Conv2d(
-            inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(
-            planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        if attention:
-            self.attn = AttentionBlock(planes)
-        else:
-            self.attn = None
-
-    def forward(self, x):
-        residual = x
-        # conv1
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        # conv2
-        out = self.conv2(out)
-        out = self.bn2(out)
-        # downsample
-        if self.downsample is not None:
-            residual = self.downsample(x)
-        # attention
-        if self.attn is not None:
-            out = self.attn(out)
-
-        # print(out.shape, residual.shape)
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-    # planes refer to the number of feature maps
-    def __init__(self, inplanes, planes, stride=1, downsample=None, attention=False):
-        super(Bottleneck, self).__init__()
-        self.downsample = downsample
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False) # kernal_size=1 don't need padding
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
-        self.relu = nn.ReLU(inplace=True)
-
-        if attention:
-            self.attn = AttentionBlock(planes * 4)
-        else:
-            self.attn = None
-
-    def forward(self, x):
-        residual = x
-        # conv1
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        # conv2
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-        # conv3
-        out = self.conv3(out)
-        out = self.bn3(out)
-        # downsample
-        if self.downsample is not None:
-            residual = self.downsample(x)
-        # attention
-        if self.attn is not None:
-            out = self.attn(out)
-
-        # print(out.shape, residual.shape)
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10, attention=False):
-        super(ResNet, self).__init__()
-        # initialize inplanes to 64, it'll be changed later
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        # layers refers to the number of blocks in each layer
-        self.layer1 = self._make_layer(block, 64, layers[0], stride=1, attention=attention)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, attention=attention)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, attention=attention)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, attention=attention)
-        # average pooling
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+class AttnVGG(nn.Module):
+    def __init__(self, sample_size, num_classes, attention=True, normalize_attn=True, init_weights=True):
+        super(AttnVGG, self).__init__()
+        # conv blocks
+        self.conv1 = self._make_layer(3, 64, 2)
+        self.conv2 = self._make_layer(64, 128, 2)
+        self.conv3 = self._make_layer(128, 256, 3)
+        self.conv4 = self._make_layer(256, 512, 3)
+        self.conv5 = self._make_layer(512, 512, 3)
+        self.conv6 = self._make_layer(512, 512, 2, pool=True)
+        self.dense = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=int(sample_size/32), padding=0, bias=True)
         # attention blocks
         self.attention = attention
         if self.attention:
-            self.attn1 = AttentionBlock(in_channels=64*block.expansion)
-            self.attn2 = AttentionBlock(in_channels=128*block.expansion)
-            self.attn3 = AttentionBlock(in_channels=256*block.expansion)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, blocks, stride, attention):
-        downsample = None
-        # when the in-channel and the out-channel dismatch, downsample!!!
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            # stride once for downsample and block.
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                    kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion)
-            )
-
-        layers = []
-        # only the first block needs downsample.
-        layers.append(block(self.inplanes, planes, stride, downsample, attention=attention))
-        # change inplanes for the next layer
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, attention=attention))
-
-        return nn.Sequential(*layers)
+            self.projector = ProjectorBlock(256, 512)
+            self.attn1 = SpatialAttn(in_features=512, normalize_attn=normalize_attn)
+            self.attn2 = SpatialAttn(in_features=512, normalize_attn=normalize_attn)
+            self.attn3 = SpatialAttn(in_features=512, normalize_attn=normalize_attn)
+        # final classification layer
+        if self.attention:
+            self.classify = nn.Linear(in_features=512*3, out_features=num_classes, bias=True)
+        else:
+            self.classify = nn.Linear(in_features=512, out_features=num_classes, bias=True)
+        if init_weights:
+            self._initialize_weights()
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
+        x = self.conv2(x)
+        l1 = self.conv3(x)
+        x = F.max_pool2d(l1, kernel_size=2, stride=2, padding=0)
+        l2 = self.conv4(x)
+        x = F.max_pool2d(l2, kernel_size=2, stride=2, padding=0)
+        l3 = self.conv5(x)
+        x = F.max_pool2d(l3, kernel_size=2, stride=2, padding=0)
+        x = self.conv6(x)
+        g = self.dense(x) # batch_sizex512x1x1
+        # attention
         if self.attention:
-            x = self.attn1(x)
-        x = self.layer2(x)
-        if self.attention:
-            x = self.attn2(x)
-        x = self.layer3(x)
-        if self.attention:
-            x = self.attn3(x)
-        x = self.layer4(x)
+            c1, g1 = self.attn1(self.projector(l1), g)
+            c2, g2 = self.attn2(l2, g)
+            c3, g3 = self.attn3(l3, g)
+            g = torch.cat((g1,g2,g3), dim=1) # batch_sizex3C
+            # classification layer
+            x = self.classify(g) # batch_sizexnum_classes
+        else:
+            c1, c2, c3 = None, None, None
+            x = self.classify(torch.squeeze(g))
+        return [x, c1, c2, c3]
 
-        g = self.avgpool(x)
-        g = g.view(g.size(0), -1)
-        x = self.fc(g)
+    def _make_layer(self, in_features, out_features, blocks, pool=False):
+        layers = []
+        for i in range(blocks):
+            conv2d = nn.Conv2d(in_channels=in_features, out_channels=out_features, kernel_size=3, padding=1)
+            layers += [conv2d, nn.BatchNorm2d(out_features), nn.ReLU(inplace=True)]
+            in_features = out_features
+            if pool:
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        return nn.Sequential(*layers)
 
-        return x
-
-    def load_my_state_dict(self, state_dict):
-        my_state_dict = self.state_dict()
-        for name, param in state_dict.items():
-            if name == 'fc.weight' or name == 'fc.bias':
-                continue
-            my_state_dict[name].copy_(param.data)
-
-model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-}
-
-def ResNet18(pretrained=False, progress=True, **kwargs):
-    model = ResNet(BasicBlock, [2,2,2,2], **kwargs)
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['resnet18'],
-            progress=progress)
-        model.load_my_state_dict(state_dict)
-    return model
-
-def ResNet34(pretrained=False, progress=True, **kwargs):
-    model = ResNet(BasicBlock, [3,4,6,3], **kwargs)
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['resnet34'],
-            progress=progress)
-        model.load_my_state_dict(state_dict)
-    return model
-
-def ResNet50(pretrained=False, progress=True, **kwargs):
-    model = ResNet(Bottleneck, [3,4,6,3], **kwargs)
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['resnet50'],
-            progress=progress)
-        model.load_my_state_dict(state_dict)
-    return model
-
-def ResNet101(pretrained=False, progress=True, **kwargs):
-    model = ResNet(Bottleneck, [3,4,23,3], **kwargs)
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['resnet101'],
-            progress=progress)
-        model.load_my_state_dict(state_dict)
-    return model
-
-def ResNet152(pretrained=False, progress=True, **kwargs):
-    model = ResNet(Bottleneck, [3,8,36,3], **kwargs)
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['resnet152'],
-            progress=progress)
-        model.load_my_state_dict(state_dict)
-    return model
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
 
 class AttnLSTM(nn.Module):
@@ -240,7 +100,7 @@ class AttnLSTM(nn.Module):
 
 # Test
 if __name__ == '__main__':
-    model = ResNet18(attention=True, num_classes=10)
+    model = AttnVGG(sample_size=128, num_classes=10)
     x = torch.randn(16,3,128,128)
     print(model(x))
     model = AttnLSTM(input_size=1, hidden_size=128, num_layers=1)
